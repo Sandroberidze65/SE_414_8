2ed5ad9fe200c1e0dae6808b6be285d9d4c054bc
alexs <sandroberidze65@gmail.com>
2026-02-03 21:19:50 +0400

    add custom student exception and handled in studnet controller change student features to trow exception when not found

--- Application/Features/Students/StudentFeatures.cs
+++ Application/Features/Students/StudentFeatures.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Dtos;
+using Application.Exceptions;
 using Application.Interfaces;
 using Domain.Model;
 
@@ -11,7 +12,7 @@ public class StudentFeatures(IStudentRepository studentRepository)
     {
         var student = await studentRepository.GetStudentsAsync();
 
-        return student.Select(s=>new StudentDto(s.Studentname, s.Lastname, s.Age)).ToList();
+        return student.Select(s => new StudentDto(s.Studentname, s.Lastname, s.Age)).ToList();
 
     }
 
@@ -19,12 +20,22 @@ public class StudentFeatures(IStudentRepository studentRepository)
     {
         var stundet = await studentRepository.GetStudentAsync(id);
 
+        if (stundet == null)
+        {
+            throw new StudentNotFoundException("Stundet Not Found");
+        }
+
         return new StudentDto(stundet.Studentname, stundet.Lastname, stundet.Age);
     }
 
     public async Task<bool> CreateStudent(StudentDto student)
     {
-        var temp = new Student() { Lastname = student.Lastname, Age = student.Age, Studentname = student.Name };
+        var temp = new Student()
+        {
+            Lastname = student.Lastname,
+            Age = student.Age,
+            Studentname = student.Name
+        };
         return await studentRepository.AddStudnetAsync(temp);
     }
 
--- Lection43/Controllers/StudentController.cs
+++ Lection43/Controllers/StudentController.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Dtos;
 using Microsoft.AspNetCore.Mvc;
 using Application.Features.Students;
+using Domain.Model;
 
 namespace Lection43.Controllers;
 
@@ -18,7 +19,18 @@ public class StudentController(StudentFeatures studentFeatures) : ControllerBase
     [HttpGet("{Id}")]
     public async Task<ActionResult<StudentDto>> Get(int Id)
     {
-        return Ok(await studentFeatures.GetStudent(Id));
+
+        try
+        {
+            var student = await studentFeatures.GetStudent(Id);
+            return Ok(student);
+        }
+        catch (Exception ex)
+        {
+
+            return BadRequest(ex.Message);
+        }
+        
     }
 
     [HttpPost]
